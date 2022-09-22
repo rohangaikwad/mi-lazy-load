6314460d0b6f46360436f81d8cddca18d94a5dae
Rohan Gaikwad <gaikwadvrohan@gmail.com>
2022-09-22 20:39:36 +0530

    bugfix

--- dist/mi-lazy-load.js
+++ dist/mi-lazy-load.js
@@ -1,35 +1,42 @@
-const InitLazyLoader = (targetClass = "lazy-load", loadedClass = "lazy-loaded") => {
-    let observer = new MutationObserver(mutations => {
-        let elems = Array.from(document.querySelectorAll("img.lazy-load:not(.lazy-loaded)"));
-        mutations.forEach(mutation => {
-            mutation.addedNodes.forEach(node => {
-                if (node.classList.contains(targetClass) && !node.classList.contains(loadedClass)) {
-                    elems.push(node);
-                }
+const MILazyLoader = {
+    init: (targetClass = "lazy-load", loadedClass = "lazy-loaded") => {
+        const ObserveImages = images => {
+            console.log(images);
+            const imageObserver = new IntersectionObserver((entries, observer) => {
+                entries.forEach(({
+                    target,
+                    isIntersecting
+                }) => {
+                    console.log(isIntersecting, target.src);
+                    if (isIntersecting && target.dataset.src && target.src.length !== target.dataset.src) {
+                        console.log("MIL LOADING", target);
+                        target.src = target.dataset.src;
+                        target.classList.add(loadedClass);
+                        imageObserver.unobserve(target);
+                    }
+                });
             });
-        });
-        ObserveImages(elems);
-    });
-    observer.observe(document.body, {
-        childList: true,
-        subtree: true
-    });
-    const ObserveImages = images => {
-        const imageObserver = new IntersectionObserver((entries, observer) => {
-            entries.forEach(({
-                target,
-                isIntersecting
-            }) => {
-                if (isIntersecting) {
-                    target.src = target.dataset.src;
-                    target.classList.add(loadedClass);
-                    imageObserver.unobserve(target);
-                }
+            images.forEach(img => {
+                img.classList.remove(targetClass);
+                imageObserver.observe(img);
             });
+        };
+        ObserveImages(Array.from(document.querySelectorAll(`img.${targetClass}:not(.${loadedClass})`)));
+        let observer = new MutationObserver(mutations => {
+            let elems = [];
+            mutations.forEach(mutation => {
+                mutation.addedNodes.forEach(node => {
+                    if (node.classList.contains(targetClass) && !node.classList.contains(loadedClass)) {
+                        console.log("MIL FOUND", node);
+                        elems.push(node);
+                    }
+                });
+            });
+            ObserveImages(elems);
         });
-        images.forEach(img => {
-            img.classList.remove(targetClass);
-            imageObserver.observe(img);
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
         });
-    };
+    }
 };
--- src/main.js
+++ src/main.js
@@ -1,23 +1,12 @@
 const MILazyLoader = {
     init: (targetClass = "lazy-load", loadedClass = "lazy-loaded") => {
-        let observer = new MutationObserver((mutations) => {
-            let elems = Array.from(document.querySelectorAll("img.lazy-load:not(.lazy-loaded)"));
-            mutations.forEach(mutation => {
-                mutation.addedNodes.forEach(node => {
-                    if(node.classList.contains(targetClass) && !node.classList.contains(loadedClass)) {
-                        elems.push(node);
-                    }
-                })
-            });
-            ObserveImages(elems);
-        });
-        observer.observe(document.body, { childList: true, subtree: true });
-        
-        
-        const ObserveImages = (images) => {    
+        const ObserveImages = (images) => {
+            console.log(images)
             const imageObserver = new IntersectionObserver((entries, observer) => {
                 entries.forEach(({target, isIntersecting}) => {
-                    if (isIntersecting) {
+                    console.log(isIntersecting,target.src)
+                    if (isIntersecting && target.dataset.src && target.src.length !== target.dataset.src ) {
+                        console.log("MIL LOADING", target)
                         target.src = target.dataset.src;
                         target.classList.add(loadedClass)
                         imageObserver.unobserve(target);
@@ -30,5 +19,21 @@ const MILazyLoader = {
                 imageObserver.observe(img)
             });
         }
+
+        ObserveImages(Array.from(document.querySelectorAll(`img.${targetClass}:not(.${loadedClass})`)));
+
+        let observer = new MutationObserver((mutations) => {
+            let elems = [];
+            mutations.forEach(mutation => {
+                mutation.addedNodes.forEach(node => {
+                    if(node.classList.contains(targetClass) && !node.classList.contains(loadedClass)) {
+                        console.log("MIL FOUND", node)
+                        elems.push(node);
+                    }
+                })
+            });
+            ObserveImages(elems);
+        });
+        observer.observe(document.body, { childList: true, subtree: true });
     }
 } 
